c8f1b3f4f10d4ff8e9f9ac1f36bd9eb4b5ecc6a7
bcid <bcid@uci.edu>
2020-06-04 07:42:56 -0700

    signup page goes straight to home page

--- app/src/main/java/com/saminaqazi/instagramclone_android_2/SignupActivity.java
+++ app/src/main/java/com/saminaqazi/instagramclone_android_2/SignupActivity.java
@@ -2,6 +2,7 @@ package com.saminaqazi.instagramclone_android_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,10 +10,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
+import com.saminaqazi.instagramclone_android_2.ParseObjects.Category;
+
+import java.util.List;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -75,10 +83,31 @@ public class SignupActivity extends AppCompatActivity {
         //user.put("phone", "650-253-0000");
         //zipcode
         user.put("location_user", zipCode);
+
         //radius
         user.put("radius", radius);
+
         //interests
-        user.put("category", ParseObject.createWithoutData("Categories", interests));
+        ParseQuery<Category> query = ParseQuery.getQuery(Category.class);
+        String objectId = "Bj4eEnT0uH"; //default walking
+        query.whereEqualTo("name", interests);
+
+        query.findInBackground(new FindCallback<Category>() {
+            public void done(List<Category> results, ParseException e) {
+                if (e != null) {
+                    // There was an error
+                    Toast.makeText(SignupActivity.this, "Added category Walking instead.", Toast.LENGTH_SHORT).show();
+                } else {
+                    // results have all the Posts the current user liked.
+                    for (ParseObject cat: results) {
+                        String objectId = cat.getObjectId().toString();
+                    }
+                }
+            }
+        });
+
+        user.put("category", ParseObject.createWithoutData("Categories", objectId));
+
 
         // Invoke signUpInBackground
         user.signUpInBackground(new SignUpCallback() {
@@ -86,6 +115,10 @@ public class SignupActivity extends AppCompatActivity {
                 if (e == null) {
                     // Hooray! Let them use the app now.
                     Toast.makeText(SignupActivity.this, "Signup Success!", Toast.LENGTH_SHORT).show();
+                    //go to main screen
+                    Intent i = new Intent(SignupActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
                 } else {
                     // Sign up didn't succeed. Look at the ParseException
                     // to figure out what went wrong
