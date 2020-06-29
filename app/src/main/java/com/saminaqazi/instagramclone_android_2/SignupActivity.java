95f1e9e3ec89f43d24cf5f7764c762180f1a481a
alejandrogznz <alejandrogzn@gmail.com>
2020-06-29 11:26:04 -0700

    Added Spinner to Register

--- app/src/main/java/com/saminaqazi/instagramclone_android_2/SignupActivity.java
+++ app/src/main/java/com/saminaqazi/instagramclone_android_2/SignupActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,6 +20,7 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 import com.saminaqazi.instagramclone_android_2.ParseObjects.Category;
+import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,8 +33,15 @@ public class SignupActivity extends AppCompatActivity {
     private EditText etEmail;
     private EditText etZipCode;
     //private EditText etRadius;
-    private EditText etInterests;
     private Button btnCreateAccount;
+    private SearchableSpinner ssRegisterCategory;
+
+    private List<String> categoryNames;
+    private List<Category> allCategories;
+
+    ArrayAdapter spinnerAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +53,16 @@ public class SignupActivity extends AppCompatActivity {
         etEmail = findViewById(R.id.etEmail);
         etZipCode = findViewById(R.id.etPostalCode);
         //etRadius = findViewById(R.id.etDistance);
-        etInterests = findViewById(R.id.etInterests);
-
+        ssRegisterCategory = findViewById(R.id.ssRegisterCategory);
         btnCreateAccount = findViewById(R.id.btnCreateAccount);
+
+        categoryNames = new ArrayList<>();
+        allCategories = new ArrayList<>();
+        spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,
+                categoryNames);
+        ssRegisterCategory.setAdapter(spinnerAdapter);
+
+        queryCategories();
 
         btnCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,7 +73,7 @@ public class SignupActivity extends AppCompatActivity {
                 String email = etEmail.getText().toString();
                 String zipCode = etZipCode.getText().toString();
                 //Float radius = Float.parseFloat(etRadius.getText().toString());
-                String interests = etInterests.getText().toString();
+                String interests = ssRegisterCategory.getSelectedItem().toString();
                 createUser(username, password, email,zipCode, interests);
                 finish();
             }
@@ -130,4 +146,33 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
     }
+
+    protected void queryCategories() {
+        ParseQuery<Category> query = ParseQuery.getQuery(Category.class);
+        //query.include(Post.KEY_USER);
+        query.addDescendingOrder(Post.KEY_CREATED_AT);
+
+        categoryNames.clear();
+
+        query.findInBackground(new FindCallback<Category>() {
+            @Override
+            public void done(List<Category> categories, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Category category : categories) {
+                    Log.i(TAG, "Category: " + category.getName() + ", Description: " + category.getDescription());
+                    allCategories.add(category);
+                    categoryNames.add(category.getName());
+                }
+                spinnerAdapter.notifyDataSetChanged();
+
+            }
+            public void onFailure(Throwable e) {
+                Log.d("DEBUG", "Fetch timeline error: " + e.toString());
+            }
+        });
+    }
+
 }
